f9ddeef16289b5983bee6407866e2c8a2cfea955
Eugene A. Kuchin <e.kuchin@gmail.com>
2022-02-19 14:45:47 +0500

    Outputs

---
+++ 02-hcl/vars-outputs/output.tf
@@ -0,0 +1,11 @@
+
+output "username" {
+  description = "This is name"
+  value = "Eugene A. Kuchin"
+}
+
+output "password" {
+  description = "This is password"
+  value = "MyP@ss01"
+  sensitive = true
+}
